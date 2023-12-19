34804ed250159fd3dd851c885badf615798b56a2
xsel <65423939+Tadjikistan@users.noreply.github.com>
2023-12-20 00:22:48 +0300

    a kak ze /

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,37 +11,37 @@ const authOptions = {
             clientSecret: process.env.DISCORD_CLIENT_SECRET,
         }),
     ],
-}
-//     callbacks: { //Получаем информацию о пользователе
-//         async signIn({user, account}) {
-//             console.log("User", user.id)
-//             console.log('Acc', account.provider)
-//             if (account.provider === 'discord') {
-//                 const {id, name, email} = user;
-//                 const balance = 300
-//                 try{
-//                     await connectMongoDB();
 
-//                     const userExists = await User.findOne({name})
-//                     if (!userExists){
-//                         const res = await fetch('/api/user', {
-//                         method: "POST",
-//                         headers: {"Content-type": "application/json",},
-//                         body: JSON.stringify({id, name, email, balance})
-//                     })
-//                     if (res.ok) {
-//                         return user;
-//                     }
-//                     }
+    callbacks: { //Получаем информацию о пользователе
+        async signIn({user, account}) {
+            console.log("User", user.id)
+            console.log('Acc', account.provider)
+            if (account.provider === 'discord') {
+                const {id, name, email} = user;
+                const balance = 300
+                try{
+                    await connectMongoDB();
+
+                    const userExists = await User.findOne({name})
+                    if (!userExists){
+                        const res = await fetch('/api/user', {
+                        method: "POST",
+                        headers: {"Content-type": "application/json",},
+                        body: JSON.stringify({id, name, email, balance})
+                    })
+                    if (res.ok) {
+                        return user;
+                    }
+                    }
                     
-//                 } catch (error){
-//                     console.log(error)
-//                 }
-//             }
-//             return user;
-//         }
-//     }
-// };
-// const handler = NextAuth(authOptions);
+                } catch (error){
+                    console.log(error)
+                }
+            }
+            return user;
+        }
+    }
+};
+const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST}
+export { handler as GET, handler as POST}
